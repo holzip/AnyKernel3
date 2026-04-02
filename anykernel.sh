d9c3f600a959813eb63e2460b6fb57407fde18ac
holzip <crutojzaur2011@gmail.com>
2026-04-02 14:00:22 +0300

    small fix anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -1,4 +1,9 @@
-# Setup
+# AnyKernel3 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# begin properties
+properties() { '
 kernel.string=depressive-kernel by holzip
 do.devicecheck=0
 do.modules=0
@@ -8,10 +13,20 @@ do.cleanuponabort=0
 device.name1=Z01R
 device.name2=ZS620KL
 device.name3=ASUS_Z01R_1
-device.name4=
-device.name5=
+'; } # end properties
 
-BLOCK=/dev/block/bootdevice/by-name/boot;
-IS_SLOT_DEVICE=1;
-RAMDISK_COMPRESSION=auto;
-PATCH_VBMETA_FLAG=auto;
+# shell variables
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=1;
+ramdisk_compression=auto;
+patch_vbmeta_flag=auto;
+
+## AnyKernel methods (static)
+# import patching functions/variables - see for reference
+. tools/ak3-core.sh;
+
+## AnyKernel install
+dump_boot;
+
+write_boot;
+## end install
